7c844cd840c90d99ada38a67a202ef133b8f17f7
Vivek <vivek9325@users.noreply.github.com>
2024-12-12 12:49:23 +0530

    Add files via upload

---
+++ module_block/azure_static_website/version.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version=">0.15"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.13.0"
+    }
+     random = {
+      source = "hashicorp/random"
+      version = "3.6.3"
+    }
+  }
+}
